98a6ec038915216e4c25a2caea9561ab9c83e49f
Rafal Lagowski <lagowski@pro4.es>
2010-09-29 19:19:11 +0200

    Dodane okno modalne z parametrem

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -22,6 +22,29 @@ class SiteController < ApplicationController
    	  format.js { render_to_facebox }
 	  end
   end
+  
+  def modalwindow3
 
+      respond_to do |format|
+     	  format.html
+     	  format.js { render_to_facebox }
+  	  end
+  end
 
+  def datamodal3
+    if request.post? 
+      if params[:texto2m].nil? or params[:texto2m].empty?
+  	    @texto2m = "NADA"
+  	  else
+  	    @texto2m = params[:texto2m]
+  	  end
+  	  
+      respond_to do |format|
+     	  format.html
+     	  format.js 
+  	  end
+    else
+      #redirect_to 
+    end
+  end
 end
